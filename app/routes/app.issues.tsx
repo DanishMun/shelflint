18dab4273fecc15f4048ddad50968d4dd6ec525e
Danish Munawar <dmunawar39@gmail.com>
2026-09-10 03:37:13 +0300

    feat(shopify): load real products from the admin api

--- app/routes/app.issues.tsx
+++ app/routes/app.issues.tsx
@@ -1,22 +1,52 @@
 
 import ScoreCard from "app/components/ScoreCard"
+import type {
+ 
+  LoaderFunctionArgs,
+} from "react-router";
+import { authenticate } from "../shopify.server";
+import { useLoaderData } from "react-router";
 
 
-const issues = [
-  { id: 1, product: "Wool Coat", problem: "Description too short" },
-  { id: 2, product: "Linen Shirt", problem: "Image has no alt text" },
-  { id: 3, product: "Denim Jacket", problem: "No SEO title" },
-]
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const {admin}=await authenticate.admin(request);
+const response = await admin.graphql(
+    `#graphql
+     query {
+  products(first: 5) {
+    nodes {
+      id
+      title
+      description
+    }
+      pageInfo {
+  hasNextPage
+  endCursor
+}
+  }
+}`)
+
+ const responseJson = await response.json();
+  return responseJson;
+};
 
 
+type Product = {
+
+    id : string;
+    title : string;
+    description : string;
+}
 
 export default function IssuesPage() {
+    const faultyProducts = useLoaderData<typeof loader>();
+    console.log(faultyProducts);
+   const products:Product[] = faultyProducts.data.products.nodes;
 return(<s-page heading="Issues">
-
     <s-section heading="product issues">
       <ScoreCard score={58} productCount={214}/>
-    {issues.map((issue)=> 
-        <s-paragraph key={issue.id}>Product Name: {issue.product} Product Issue: {issue.problem} </s-paragraph>
+    {products.map((productf)=> 
+        <s-paragraph key={productf.id}>Product Name: {productf.title}  </s-paragraph>
     )}
     </s-section>
 </s-page>)
